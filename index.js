dae0551c001a007c63f24d9282f1e9b006356d0b
Peywan Sabir <90625487+peywan@users.noreply.github.com>
2022-08-26 01:20:28 +0200

    added clock

---
+++ index.js
@@ -0,0 +1,21 @@
+function displayTime(){
+    const dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();{}
+    let session = document.getElementById('session'); {
+        
+    }
+
+
+    if(hrs >= 12) {
+        session.innerHTML = 'PM';
+    }else {
+        session.innerHTML ='AM'
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+setInterval(displayTime, 10);
---
+++ jsclock.js
@@ -0,0 +1,8 @@
+/*
+Follow me on Twitter : @One_div
+or on Dribbble : dribbble.com/One_div
+*/
+
+$('#range').on("change", function() {
+    $('.wrapper').css('font-size',this.value +"px");
+    }).trigger("change");
